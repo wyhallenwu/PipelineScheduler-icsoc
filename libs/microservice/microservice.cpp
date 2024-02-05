2b74e3f77ceb70f525b05850ba30e0e8c2779ff4
Lucas Liebe <lucasliebe.ll@gmail.com>
2024-02-05 17:33:43 +0900

    feat: Structure Microservice Classes

--- libs/microservice/microservice.cpp
+++ libs/microservice/microservice.cpp
@@ -1,6 +1,7 @@
 #include<microservice.h>
 #include<iostream>
 #include<opencv2/opencv.hpp>
+
 /**
  * @brief 
  * 
@@ -8,79 +9,79 @@
  * @param isInQueue 
  * @return QueueLengthType 
  */
-NumQueuesType Microservice::generateQueue(const QueueType queueType, bool isInQueue) {
-    QueueLengthType queueNum = -1;
-    switch (queueType) {
-        case QueueType::gpuDataQueue: {
-            std::queue<GPUDataRequest> newQueue;
-            if (isInQueue) {
-                numGPUInQueues++;
-                gpuInQueueList.emplace_back(newQueue);
-                queueNum = gpuInQueueList.size() - 1;
-            } else {
-                numGPUOutQueues++;
-                gpuOutQueueList.emplace_back(newQueue);
-                queueNum = gpuOutQueueList.size() - 1;
-            }
-            break;
-        }
-        case QueueType::shmDataQueue: {
-            std::queue<DataRequest<ShmReqDataType>> newQueue;
-            if (isInQueue) {
-                numShmInQueues++;
-                shmInQueueList.emplace_back(newQueue);
-                queueNum = shmInQueueList.size() - 1;
-            } else {
-                numShmOutQueues++;
-                shmOutQueueList.emplace_back(newQueue);
-                queueNum = shmOutQueueList.size() - 1;
-            }
-            break;
-        }
-        case QueueType::cpuDataQueue: {
-            std::queue<DataRequest<CPUReqDataType>> *newQueue;
-            std::vector<void*> newQueueList;
-            newQueueList.emplace_back(newQueue)
-            const cv::dnn::dnn4_v20230620::MatShape size = {1, 3, 640, 640};
-            cv::Mat mat = cv::Mat(size, CV_32F, 3);
-            ClockType time = 1;
-            DataRequest<CPUReqDataType> req = DataRequest<CPUReqDataType>(time, 1, size, "", mat);
-            &newQueueList[0].emplace_back(req);
-            std::cout << &newQueueList[0].size();
-            if (isInQueue) {
-                numCPUInQueues++;
-                cpuInQueueList.emplace_back(newQueue);
-                queueNum = cpuInQueueList.size() - 1;
-            } else {
-                numCPUOutQueues++;
-                cpuOutQueueList.emplace_back(newQueue);
-                queueNum = cpuOutQueueList.size() - 1;
-            }
-            break;
-        }
-        default:
-            break;
-    }
-    return queueNum;
-}
+//template<typename InType>
+//NumQueuesType GPUDataMicroservice<InType>::generateQueue(const QueueType queueType, bool isInQueue) {
+//    QueueLengthType queueNum = -1;
+//    if (isInQueue) {
+//        std::queue<InType> newQueue;
+//        numGPUInQueues++;
+//        InQueue = newQueue;
+//        queueNum += 1;
+//    } else {
+//        std::queue<GPUDataRequest> newQueue;
+//        numGPUOutQueues++;
+//        OutQueue = newQueue;
+//        queueNum = OutQueue.size() - 1;
+//    }
+//    return queueNum;
+//}
+//
+//template<typename InType>
+//NumQueuesType ShMemMicroservice<ShmReqDataType>::generateQueue(const QueueType queueType, bool isInQueue) {
+//    QueueLengthType queueNum = -1;
+//    if (isInQueue) {
+//        std::queue<InType> newQueue;
+//        numGPUInQueues++;
+//        InQueue = newQueue;
+//        queueNum += 1;
+//    } else {
+//        std::queue<DataRequest<ShmReqDataType>> newQueue;
+//        numGPUOutQueues++;
+//        OutQueue = newQueue;
+//        queueNum = OutQueue.size() - 1;
+//    }
+//    return queueNum;
+//}
+//
+//template<typename InType>
+//NumQueuesType SerDataMicroservice<InType>::generateQueue(const QueueType queueType, bool isInQueue) {
+//    QueueLengthType queueNum = -1;
+//    std::queue<DataRequest<CPUReqDataType>> *newQueue;
+//    const cv::dnn::dnn4_v20230620::MatShape size = {1, 3, 640, 640};
+//    cv::Mat mat = cv::Mat(size, CV_32F, 3);
+//    ClockType time = 1;
+//    DataRequest<CPUReqDataType> req = DataRequest<CPUReqDataType>(time, 1, size, "", mat);
+//    newQueue->push(req);
+//    if (isInQueue) {
+//        numGPUInQueues++;
+//        InQueue = *newQueue;
+//        queueNum += 1;
+//    } else {
+//        numGPUOutQueues++;
+//        OutQueue = *newQueue;
+//        queueNum = OutQueue.size() - 1;
+//    }
+//    return queueNum;
+//}
 
-Microservice::Microservice(const BaseMicroserviceConfigs& configs) {
+template<typename InType>
+Microservice<InType>::Microservice(const BaseMicroserviceConfigs &configs) {
     msvc_name = configs.msvc_name;
     msvc_svcLevelObjLatency = configs.msvc_svcLevelObjLatency;
 
 
     std::list<NeighborMicroserviceConfigs>::const_iterator it;
-    for (it = configs.upstreamMicroservices.begin();it != configs.upstreamMicroservices.end(); ++it) {
+    for (it = configs.upstreamMicroservices.begin(); it != configs.upstreamMicroservices.end(); ++it) {
         numDnstreamMicroservices++;
         // NeighborMicroservice
-        // For each upstream microservice, a 
+        // For each upstream microservice, a
         // switch (it->commMethod) {
         // case CommMethod::gRPC:
         //     break;
 
         // case CommMethod::gRPCLocal:
         //     break;
-        
+
         // case CommMethod::sharedMemory:
         //     break;
         // case CommMethod::localQueue:
@@ -103,7 +104,7 @@ Microservice::Microservice(const BaseMicroserviceConfigs& configs) {
 
     //     case CommMethod::gRPCLocal:
     //         break;
-        
+
     //     case CommMethod::sharedMemory:
     //         break;
     //     case CommMethod::localQueue:
@@ -115,3 +116,66 @@ Microservice::Microservice(const BaseMicroserviceConfigs& configs) {
     //     }
     // }
 }
+
+template<typename InType>
+GPUDataMicroservice<InType>::GPUDataMicroservice(const BaseMicroserviceConfigs &configs)
+        :Microservice<InType>(configs) {
+    OutQueue = std::queue<GPUDataRequest>();
+}
+
+template<typename InType>
+ShMemMicroservice<InType>::ShMemMicroservice(const BaseMicroserviceConfigs &configs)
+        :Microservice<InType>(configs) {
+    OutQueue = std::queue<DataRequest<ShmReqDataType>>();
+}
+
+template<typename InType>
+SerDataMicroservice<InType>::SerDataMicroservice(const BaseMicroserviceConfigs &configs)
+        :Microservice<InType>(configs) {
+    OutQueue = std::queue<DataRequest<CPUReqDataType>>();
+}
+
+template<typename InType>
+void Microservice<InType>::Schedule() {
+    if (InQueue->empty()) {
+        return;
+    }
+    InType data = InQueue->front();
+    InQueue->pop();
+    // process data
+    // No out queue as this is only used for final job
+}
+
+template<typename InType>
+void GPUDataMicroservice<InType>::Schedule() {
+    if (Microservice<InType>::InQueue->empty()) {
+        return;
+    }
+    InType data = Microservice<InType>::InQueue->front();
+    Microservice<InType>::InQueue->pop();
+    // process data
+    OutQueue.push(data);
+}
+
+template<typename InType>
+void ShMemMicroservice<InType>::Schedule() {
+    if (Microservice<InType>::InQueue->empty()) {
+        return;
+    }
+    InType data = Microservice<InType>::InQueue->front();
+    Microservice<InType>::InQueue->pop();
+    // process data
+    OutQueue.push(data);
+}
+
+template<typename InType>
+void SerDataMicroservice<InType>::Schedule() {
+    if (Microservice<InType>::InQueue->empty()) {
+        return;
+    }
+    InType data = Microservice<InType>::InQueue->front();
+    Microservice<InType>::InQueue->pop();
+    // process data
+    OutQueue.push(data);
+}
+
