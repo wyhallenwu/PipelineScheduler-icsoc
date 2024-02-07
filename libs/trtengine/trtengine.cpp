b537212d447e902f39898d4fc94d1f52901eeb7c
tungngreen <29793179+tungngreen@users.noreply.github.com>
2024-02-08 01:17:46 +0900

    Minor changes

--- libs/trtengine/trtengine.cpp
+++ libs/trtengine/trtengine.cpp
@@ -12,14 +12,6 @@
 using namespace nvinfer1;
 using namespace Util;
 
-std::vector<std::string> Util::getFilesInDirectory(const std::string& dirPath) {
-    std::vector<std::string> filepaths;
-    for (const auto& entry: std::filesystem::directory_iterator(dirPath)) {
-        filepaths.emplace_back(entry.path().string());
-    }
-    return filepaths;
-}
-
 void Logger::log(Severity severity, const char *msg) noexcept {
     // Would advise using a proper logging utility such as https://github.com/gabime/spdlog
     // For the sake of this tutorial, will just log to the console.
