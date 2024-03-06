2d9b78670b710898aee07727c4261bf271cd8d6e
Lucas Liebe <lucasliebe.ll@gmail.com>
2024-03-06 20:18:38 +0900

    fix: typo in number

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "receiver.h"
 
 int main() {
-    auto r = new Receiver({"dummy_receiver_0", MicroserviceType::Receiver, 0, 1, {}, {{"datasource_0", CommMethod::sharedMemory, {"localhost:5000"}, 0, -2, {{0,0}}}},{{"dummy", CommMethod::localCPU, {""}, 0, -1, {{0,0}}}}}, CommMethod::localCPU);
+    auto r = new Receiver({"dummy_receiver_0", MicroserviceType::Receiver, 0, 1, {}, {{"datasource_0", CommMethod::sharedMemory, {"localhost:55000"}, 0, -2, {{0,0}}}},{{"dummy", CommMethod::localCPU, {""}, 0, -1, {{0,0}}}}}, CommMethod::localCPU);
     while (true){
         std::cout << "QueueSize = " << r->GetOutQueueSize(0) << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
