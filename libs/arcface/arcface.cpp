d2a6f873769ca594f7445eda7e9cfc8202a36ddc
Lucas Liebe <lucasliebe.ll@gmail.com>
2024-03-20 16:57:32 +0900

    feat: add ArcFaceAgent

---
+++ libs/arcface/arcface.cpp
@@ -0,0 +1,48 @@
+#include "arcface.h"
+
+ArcFaceAgent::ArcFaceAgent(const std::string &name, uint16_t own_port, std::vector<Microservice *> services)
+        : ContainerAgent(name, own_port) {
+    msvcs = std::move(services);
+    std::thread preprocessor(&ArcFacePreprocessor::batchRequests, dynamic_cast<ArcFacePreprocessor*>(msvcs[1]));
+    preprocessor.detach();
+    std::thread inference(&ArcFaceInference::inference, dynamic_cast<ArcFaceInference*>(msvcs[2]));
+    inference.detach();
+    std::thread postprocessor(&ArcFacePostprocessor::postProcessing, dynamic_cast<ArcFacePostprocessor*>(msvcs[3]));
+    postprocessor.detach();
+    for (int i = 4; i < msvcs.size(); i++) {
+        std::thread sender(&Sender::Process, dynamic_cast<Sender*>(msvcs[i]));
+        sender.detach();
+    }
+}
+
+int main(int argc, char **argv) {
+    absl::ParseCommandLine(argc, argv);
+    std::vector<BaseMicroserviceConfigs> msvc_configs = msvcconfigs::LoadFromJson();
+    TRTConfigs yoloConfigs = json::parse(absl::GetFlag(FLAGS_trt_json).value()).get<TRTConfigs>();
+    std::string name = absl::GetFlag(FLAGS_name);
+    std::vector<Microservice*> msvcs;
+    msvcs.push_back(new Receiver(msvc_configs[0], CommMethod::localGPU));
+    msvcs.push_back(new ArcFacePreprocessor(msvc_configs[1]));
+    msvcs[1]->SetInQueue(msvcs[0]->GetOutQueue());
+    msvcs.push_back(new ArcFaceInference(msvc_configs[2], yoloConfigs));
+    msvcs[2]->SetInQueue(msvcs[1]->GetOutQueue());
+    msvcs.push_back(new ArcFacePostprocessor(msvc_configs[3]));
+    msvcs[3]->SetInQueue(msvcs[2]->GetOutQueue());
+    for (int i = 4; i < msvc_configs.size(); i++) {
+        if (msvc_configs[i].dnstreamMicroservices.front().commMethod == CommMethod::localGPU) {
+            msvcs.push_back(new GPUSender(msvc_configs[i]));
+        } else if (msvc_configs[i].dnstreamMicroservices.front().commMethod == CommMethod::sharedMemory) {
+            msvcs.push_back(new LocalCPUSender(msvc_configs[i]));
+        } else if (msvc_configs[i].dnstreamMicroservices.front().commMethod == CommMethod::sharedMemory) {
+            msvcs.push_back(new RemoteCPUSender(msvc_configs[i]));
+        }
+        msvcs[i]->SetInQueue(msvcs[i - 1]->GetOutQueue());
+    }
+    ContainerAgent *agent = new ArcFaceAgent(name, absl::GetFlag(FLAGS_port), msvcs);
+    while (agent->running()) {
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+        agent->SendQueueLengths();
+    }
+    delete agent;
+    return 0;
+}
--- libs/arcface/arcface.h
+++ libs/arcface/arcface.h
@@ -1,12 +1,16 @@
 #include <baseprocessor.h>
 #include <trtengine.h>
 #include <misc.h>
+#include "container_agent.h"
+#include "receiver.h"
+#include "sender.h"
 
 class ArcFacePreprocessor : public BasePreprocessor {
 public:
     ArcFacePreprocessor(const BaseMicroserviceConfigs &config);
-    ~ArcFacePreprocessor();
+    ~ArcFacePreprocessor() = default;
 protected:
+    friend class ArcFaceAgent;
     void batchRequests();
     // 
     // bool isTimeToBatch() override;
@@ -19,8 +23,9 @@ protected:
 class ArcFaceInference : public BaseProcessor {
 public:
     ArcFaceInference(const BaseMicroserviceConfigs &config, const TRTConfigs &engineConfigs);
-    ~ArcFaceInference();
+    ~ArcFaceInference() = default;
 protected:
+    friend class ArcFaceAgent;
     void inference();
     std::vector<void *> msvc_engineInputBuffers, msvc_engineOutputBuffers;
     TRTConfigs msvc_engineConfigs;
@@ -30,7 +35,13 @@ protected:
 class ArcFacePostprocessor : public BasePostprocessor {
 public:
     ArcFacePostprocessor(const BaseMicroserviceConfigs &config);
-    ~ArcFacePostprocessor();
+    ~ArcFacePostprocessor() = default;
 protected:
+    friend class ArcFaceAgent;
     void postProcessing();
+};
+
+class ArcFaceAgent : public ContainerAgent {
+public:
+    ArcFaceAgent(const std::string &name, uint16_t own_port, std::vector<Microservice*> services);
 };
