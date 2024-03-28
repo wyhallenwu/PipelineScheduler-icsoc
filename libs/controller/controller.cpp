197a998920e594feaf7b2013c1beb80f28945fdc
Lucas Liebe <lucasliebe.ll@gmail.com>
2024-03-28 15:03:43 +0900

    feat: add Controller skeleton

---
+++ libs/controller/controller.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lucas on 24. 3. 28.
+//
+
+#include "controller.h"
---
+++ libs/controller/controller.h
@@ -0,0 +1,14 @@
+//
+// Created by lucas on 24. 3. 28.
+//
+
+#ifndef PIPEPLUSPLUS_CONTROLLER_H
+#define PIPEPLUSPLUS_CONTROLLER_H
+
+
+class controller {
+
+};
+
+
+#endif //PIPEPLUSPLUS_CONTROLLER_H
