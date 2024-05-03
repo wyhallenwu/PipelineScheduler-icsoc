0e48de0a672f2031c3142c8f76bc8b2878a89f64
tungngreen <29793179+tungngreen@users.noreply.github.com>
2024-05-03 15:27:49 +0900

    - Fix time to vector of vector - Unpause sender thread

--- libs/communicator/communicator.cpp
+++ libs/communicator/communicator.cpp
@@ -3,6 +3,7 @@
 #include "receiver.h"
 #include "sender.h"
 #include "absl/flags/flag.h"
+#include "spdlog/spdlog.h"
 
 ABSL_FLAG(std::string, type, "", "type of communicator to start");
 ABSL_FLAG(int, size, 0, "size of the quadratic image matrix");
@@ -18,18 +19,19 @@ void run_receiver(Receiver &receiver, bool gpu, int size) {
             if (strcmp(tmp.req_travelPath[0].c_str(), "empty") == 0) {
                 continue;
             }
-            file << std::chrono::duration_cast<std::chrono::milliseconds>(tmp.req_origGenTime[0] - std::chrono::high_resolution_clock::now()).count() << std::endl;
+            file << std::chrono::duration_cast<std::chrono::milliseconds>(tmp.req_origGenTime[0][0] - std::chrono::high_resolution_clock::now()).count() << std::endl;
         } else {
             auto tmp = queue->pop1();
             if (strcmp(tmp.req_travelPath[0].c_str(), "empty") == 0) {
                 continue;
             }
-            file << std::chrono::duration_cast<std::chrono::milliseconds>(tmp.req_origGenTime[0] - std::chrono::high_resolution_clock::now()).count() << std::endl;
+            file << std::chrono::duration_cast<std::chrono::milliseconds>(tmp.req_origGenTime[0][0] - std::chrono::high_resolution_clock::now()).count() << std::endl;
         }
         i++;
     }
     file.close();
 }
+// Time stamp is a vector of vector now
 
 int main(int argc, char *argv[]) {
     absl::ParseCommandLine(argc, argv);
@@ -59,10 +61,11 @@ int main(int argc, char *argv[]) {
         run_receiver(receiver, false, size);
     } else {
         auto *queue = new ThreadSafeFixSizedDoubleQueue(100, -1);
+        Sender *sender;
         bool gpu = false;
         if (type == "gpu-sender") {
             gpu = true;
-            GPUSender sender = GPUSender(json::parse(
+            sender = new GPUSender(json::parse(
                     "{\"msvc_name\": \"sender\", \"msvc_contName\": \"sender\", \"msvc_idealBatchSize\": 20, \"msvc_appLvlConfigs\": \"\", "
                     "\"msvc_dnstreamMicroservices\": [{\"nb_classOfInterest\": -1, \"nb_commMethod\": 3, \"nb_link\": [\"" +
                     absl::GetFlag(FLAGS_url) +
@@ -71,10 +74,10 @@ int main(int argc, char *argv[]) {
                     "\"msvc_upstreamMicroservices\": [{\"nb_classOfInterest\": -2, \"nb_commMethod\": 3, \"nb_link\": [\"\"], "
                     "\"nb_maxQueueSize\": 10, \"nb_name\": \"::postprocessor\", \"nb_expectedShape\": [[-1, -1]]}], "
                     "\"msvc_maxQueueSize\": 100, \"msvc_deviceIndex\": 0, \"msvc_containerLogPath\" : \"\", \"msvc_RUNMODE\" : 0}"));
-            sender.SetInQueue({queue});
-            sender.dispatchThread();
+            sender->SetInQueue({queue});
+            sender->dispatchThread();
         } else if (type == "local-cpu-sender") {
-            LocalCPUSender sender = LocalCPUSender(json::parse(
+            sender = new LocalCPUSender(json::parse(
                     "{\"msvc_name\": \"sender\", \"msvc_contName\": \"sender\", \"msvc_idealBatchSize\": 20, \"msvc_appLvlConfigs\": \"\", "
                     "\"msvc_dnstreamMicroservices\": [{\"nb_classOfInterest\": -1, \"nb_commMethod\": 3, \"nb_link\": [\"" +
                     absl::GetFlag(FLAGS_url) +
@@ -83,10 +86,10 @@ int main(int argc, char *argv[]) {
                     "\"msvc_upstreamMicroservices\": [{\"nb_classOfInterest\": -2, \"nb_commMethod\": 3, \"nb_link\": [\"\"], "
                     "\"nb_maxQueueSize\": 10, \"nb_name\": \"::postprocessor\", \"nb_expectedShape\": [[-1, -1]]}], "
                     "\"msvc_maxQueueSize\": 100, \"msvc_deviceIndex\": 0, \"msvc_containerLogPath\" : \"\", \"msvc_RUNMODE\" : 0}"));
-            sender.SetInQueue({queue});
-            sender.dispatchThread();
+            sender->SetInQueue({queue});
+            sender->dispatchThread();
         } else if (type == "remote-cpu-sender") {
-            RemoteCPUSender sender = RemoteCPUSender(json::parse(
+            sender = new RemoteCPUSender(json::parse(
                     "{\"msvc_name\": \"sender\", \"msvc_contName\": \"sender\", \"msvc_idealBatchSize\": 20, \"msvc_appLvlConfigs\": \"\", "
                     "\"msvc_dnstreamMicroservices\": [{\"nb_classOfInterest\": -1, \"nb_commMethod\": 3, \"nb_link\": [\"" +
                     absl::GetFlag(FLAGS_url) +
@@ -95,14 +98,16 @@ int main(int argc, char *argv[]) {
                     "\"msvc_upstreamMicroservices\": [{\"nb_classOfInterest\": -2, \"nb_commMethod\": 3, \"nb_link\": [\"\"], "
                     "\"nb_maxQueueSize\": 10, \"nb_name\": \"::postprocessor\", \"nb_expectedShape\": [[-1, -1]]}], "
                     "\"msvc_maxQueueSize\": 100, \"msvc_deviceIndex\": 0, \"msvc_containerLogPath\" : \"\", \"msvc_RUNMODE\" : 0}"));
-            sender.SetInQueue({queue});
-            sender.dispatchThread();
+            sender->SetInQueue({queue});
+            sender->dispatchThread();
         } else {
             std::cerr << "Invalid type of communicator" << std::endl;
         }
         while (true) {
             std::cin >> type;
             if (type == "start") {
+                // Needs this to make sender start running
+                sender->PAUSE_THREADS = false;
                 break;
             }
         }
@@ -110,19 +115,21 @@ int main(int argc, char *argv[]) {
             cv::cuda::GpuMat img;
             img.create(size, size, CV_8UC3);
             img.setTo(cv::Scalar::all(0));
-            Request<LocalGPUReqDataType> payload = {{std::chrono::high_resolution_clock::now()}, {0}, {""}, 0,
+            Request<LocalGPUReqDataType> payload = {{{std::chrono::high_resolution_clock::now()}}, {0}, {""}, 0,
                                                     {{{3, size, size}, img}}};
             for (int i = 0; i < 100; i++) {
-                payload.req_origGenTime = {std::chrono::high_resolution_clock::now()};
+                spdlog::info("Sending image {}", i);
+                payload.req_origGenTime = {{std::chrono::high_resolution_clock::now()}};
                 queue->emplace(payload);
                 std::this_thread::sleep_for(std::chrono::milliseconds(500));
             }
         } else {
             cv::Mat img = cv::Mat::zeros(size, size, CV_8UC3);
-            Request<LocalCPUReqDataType> payload = {{std::chrono::high_resolution_clock::now()}, {0}, {""}, 0,
+            Request<LocalCPUReqDataType> payload = {{{std::chrono::high_resolution_clock::now()}}, {0}, {""}, 0,
                                                     {{{3, size, size}, img}}};
             for (int i = 0; i < 100; i++) {
-                payload.req_origGenTime = {std::chrono::high_resolution_clock::now()};
+                spdlog::info("Sending image {}", i);
+                payload.req_origGenTime = {{std::chrono::high_resolution_clock::now()}};
                 queue->emplace(payload);
                 std::this_thread::sleep_for(std::chrono::milliseconds(500));
             }
