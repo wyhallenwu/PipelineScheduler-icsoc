10f7fed95d43b3ca2077a73c112fd8239a120954
tungngreen <29793179+tungngreen@users.noreply.github.com>
2024-07-03 13:36:11 +0900

    Minor changes

--- libs/controller/scheduling-ppp.cpp
+++ libs/controller/scheduling-ppp.cpp
@@ -247,255 +247,6 @@ void Controller::ApplyScheduling() {
     }
 }
 
-PipelineModelListType Controller::getModelsByPipelineType(PipelineType type, const std::string &startDevice) {
-    switch (type) {
-        case PipelineType::Traffic: {
-            auto *datasource = new PipelineModel{startDevice, "datasource", {}, true, {}, {}};
-            datasource->possibleDevices = {startDevice};
-
-            auto *yolov5n = new PipelineModel{
-                    "server",
-                    "yolov5n",
-                    {},
-                    true,
-                    {},
-                    {},
-                    {},
-                    {{datasource, -1}}
-            };
-            yolov5n->possibleDevices = {startDevice, "server"};
-            datasource->downstreams.push_back({yolov5n, -1});
-
-            auto *retina1face = new PipelineModel{
-                    "server",
-                    "retina1face",
-                    {},
-                    false,
-                    {},
-                    {},
-                    {},
-                    {{yolov5n, 0}}
-            };
-            retina1face->possibleDevices = {startDevice, "server"};
-            yolov5n->downstreams.push_back({retina1face, 0});
-
-            auto *carbrand = new PipelineModel{
-                    "server",
-                    "carbrand",
-                    {},
-                    false,
-                    {},
-                    {},
-                    {},
-                    {{yolov5n, 2}}
-            };
-            yolov5n->downstreams.push_back({carbrand, 2});
-
-            auto *platedet = new PipelineModel{
-                    "server",
-                    "platedet",
-                    {},
-                    false,
-                    {},
-                    {},
-                    {},
-                    {{yolov5n, 2}}
-            };
-            yolov5n->downstreams.push_back({platedet, 2});
-
-            auto *sink = new PipelineModel{
-                    "server",
-                    "sink",
-                    {},
-                    false,
-                    {},
-                    {},
-                    {},
-                    {{retina1face, -1}, {carbrand, -1}, {platedet, -1}}
-            };
-            sink->possibleDevices = {"server"};
-            retina1face->downstreams.push_back({sink, -1});
-            carbrand->downstreams.push_back({sink, -1});
-            platedet->downstreams.push_back({sink, -1});
-
-            return {datasource, yolov5n, retina1face, carbrand, platedet, sink};
-        }
-        case PipelineType::Building_Security: {
-            auto *datasource = new PipelineModel{startDevice, "datasource", {}, true, {}, {}};
-            datasource->possibleDevices = {startDevice};
-            auto *yolov5n = new PipelineModel{
-                    "server",
-                    "yolov5n",
-                    {},
-                    true,
-                    {},
-                    {},
-                    {},
-                    {{datasource, -1}}
-            };
-            yolov5n->possibleDevices = {startDevice, "server"};
-            datasource->downstreams.push_back({yolov5n, -1});
-
-            auto *retina1face = new PipelineModel{
-                    "server",
-                    "retina1face",
-                    {},
-                    false,
-                    {},
-                    {},
-                    {},
-                    {{yolov5n, 0}}
-            };
-            retina1face->possibleDevices = {startDevice, "server"};
-            yolov5n->downstreams.push_back({retina1face, 0});
-
-            auto *movenet = new PipelineModel{
-                    "server",
-                    "movenet",
-                    {},
-                    false,
-                    {},
-                    {},
-                    {},
-                    {{yolov5n, 0}}
-            };
-            movenet->possibleDevices = {"server"};
-            yolov5n->downstreams.push_back({movenet, 0});
-
-            auto *gender = new PipelineModel{
-                    "server",
-                    "gender",
-                    {},
-                    false,
-                    {},
-                    {},
-                    {},
-                    {{retina1face, -1}}
-            };
-            gender->possibleDevices = {"server"};
-            retina1face->downstreams.push_back({gender, -1});
-
-            auto *age = new PipelineModel{
-                    "server",
-                    "age",
-                    {},
-                    false,
-                    {},
-                    {},
-                    {},
-                    {{retina1face, -1}}
-            };
-            age->possibleDevices = {"server"};
-            retina1face->downstreams.push_back({age, -1});
-
-            auto *sink = new PipelineModel{
-                    "server",
-                    "sink",
-                    {},
-                    false,
-                    {},
-                    {},
-                    {},
-                    {{gender, -1}, {age, -1}, {movenet, -1}}
-            };
-            sink->possibleDevices = {"server"};
-            gender->downstreams.push_back({sink, -1});
-            age->downstreams.push_back({sink, -1});
-            movenet->downstreams.push_back({sink, -1});
-
-            return {datasource, yolov5n, retina1face, movenet, gender, age, sink};
-        }
-        case PipelineType::Video_Call: {
-            auto *datasource = new PipelineModel{startDevice, "datasource", {}, true, {}, {}};
-            datasource->possibleDevices = {startDevice};
-            auto *retina1face = new PipelineModel{
-                    "server",
-                    "retina1face",
-                    {},
-                    true,
-                    {},
-                    {},
-                    {},
-                    {{datasource, -1}}
-            };
-            retina1face->possibleDevices = {startDevice, "server"};
-            datasource->downstreams.push_back({retina1face, -1});
-
-            auto *emotionnet = new PipelineModel{
-                    "server",
-                    "emotionnet",
-                    {},
-                    false,
-                    {},
-                    {},
-                    {},
-                    {{retina1face, -1}}
-            };
-            emotionnet->possibleDevices = {"server"};
-            retina1face->downstreams.push_back({emotionnet, -1});
-
-            auto *age = new PipelineModel{
-                    "server",
-                    "age",
-                    {},
-                    false,
-                    {},
-                    {},
-                    {},
-                    {{retina1face, -1}}
-            };
-            age->possibleDevices = {startDevice, "server"};
-            retina1face->downstreams.push_back({age, -1});
-
-            auto *gender = new PipelineModel{
-                    "server",
-                    "gender",
-                    {},
-                    false,
-                    {},
-                    {},
-                    {},
-                    {{retina1face, -1}}
-            };
-            gender->possibleDevices = {startDevice, "server"};
-            retina1face->downstreams.push_back({gender, -1});
-
-            auto *arcface = new PipelineModel{
-                    "server",
-                    "arcface",
-                    {},
-                    false,
-                    {},
-                    {},
-                    {},
-                    {{retina1face, -1}}
-            };
-            arcface->possibleDevices = {"server"};
-            retina1face->downstreams.push_back({arcface, -1});
-
-            auto *sink = new PipelineModel{
-                    "server",
-                    "sink",
-                    {},
-                    false,
-                    {},
-                    {},
-                    {},
-                    {{emotionnet, -1}, {age, -1}, {gender, -1}, {arcface, -1}}
-            };
-            sink->possibleDevices = {"server"};
-            emotionnet->downstreams.push_back({sink, -1});
-            age->downstreams.push_back({sink, -1});
-            gender->downstreams.push_back({sink, -1});
-            arcface->downstreams.push_back({sink, -1});
-
-            return {datasource, retina1face, emotionnet, age, gender, arcface, sink};
-        }
-        default:
-            return {};
-    }
-}
-
 bool Controller::mergeArrivalProfiles(ModelArrivalProfile &mergedProfile, const ModelArrivalProfile &toBeMergedProfile) {
     mergedProfile.arrivalRates += toBeMergedProfile.arrivalRates;
     auto mergedD2DProfile = &mergedProfile.d2dNetworkProfile;
@@ -849,4 +600,258 @@ uint64_t Controller::calculateQueuingLatency(const float &arrival_rate, const fl
     float numQueriesInSystem = rho / (1 - rho);
     float averageQueueLength = rho * rho / (1 - rho);
     return (uint64_t) (averageQueueLength / arrival_rate * 1000000);
+}
+
+// ============================================================================================================================================
+// ============================================================================================================================================
+// ============================================================================================================================================
+// ============================================================================================================================================
+
+PipelineModelListType Controller::getModelsByPipelineType(PipelineType type, const std::string &startDevice) {
+    switch (type) {
+        case PipelineType::Traffic: {
+            auto *datasource = new PipelineModel{startDevice, "datasource", {}, true, {}, {}};
+            datasource->possibleDevices = {startDevice};
+
+            auto *yolov5n = new PipelineModel{
+                    "server",
+                    "yolov5n",
+                    {},
+                    true,
+                    {},
+                    {},
+                    {},
+                    {{datasource, -1}}
+            };
+            yolov5n->possibleDevices = {startDevice, "server"};
+            datasource->downstreams.push_back({yolov5n, -1});
+
+            auto *retina1face = new PipelineModel{
+                    "server",
+                    "retina1face",
+                    {},
+                    false,
+                    {},
+                    {},
+                    {},
+                    {{yolov5n, 0}}
+            };
+            retina1face->possibleDevices = {startDevice, "server"};
+            yolov5n->downstreams.push_back({retina1face, 0});
+
+            auto *carbrand = new PipelineModel{
+                    "server",
+                    "carbrand",
+                    {},
+                    false,
+                    {},
+                    {},
+                    {},
+                    {{yolov5n, 2}}
+            };
+            yolov5n->downstreams.push_back({carbrand, 2});
+
+            auto *platedet = new PipelineModel{
+                    "server",
+                    "platedet",
+                    {},
+                    false,
+                    {},
+                    {},
+                    {},
+                    {{yolov5n, 2}}
+            };
+            yolov5n->downstreams.push_back({platedet, 2});
+
+            auto *sink = new PipelineModel{
+                    "server",
+                    "sink",
+                    {},
+                    false,
+                    {},
+                    {},
+                    {},
+                    {{retina1face, -1}, {carbrand, -1}, {platedet, -1}}
+            };
+            sink->possibleDevices = {"server"};
+            retina1face->downstreams.push_back({sink, -1});
+            carbrand->downstreams.push_back({sink, -1});
+            platedet->downstreams.push_back({sink, -1});
+
+            return {datasource, yolov5n, retina1face, carbrand, platedet, sink};
+        }
+        case PipelineType::Building_Security: {
+            auto *datasource = new PipelineModel{startDevice, "datasource", {}, true, {}, {}};
+            datasource->possibleDevices = {startDevice};
+            auto *yolov5n = new PipelineModel{
+                    "server",
+                    "yolov5n",
+                    {},
+                    true,
+                    {},
+                    {},
+                    {},
+                    {{datasource, -1}}
+            };
+            yolov5n->possibleDevices = {startDevice, "server"};
+            datasource->downstreams.push_back({yolov5n, -1});
+
+            auto *retina1face = new PipelineModel{
+                    "server",
+                    "retina1face",
+                    {},
+                    false,
+                    {},
+                    {},
+                    {},
+                    {{yolov5n, 0}}
+            };
+            retina1face->possibleDevices = {startDevice, "server"};
+            yolov5n->downstreams.push_back({retina1face, 0});
+
+            auto *movenet = new PipelineModel{
+                    "server",
+                    "movenet",
+                    {},
+                    false,
+                    {},
+                    {},
+                    {},
+                    {{yolov5n, 0}}
+            };
+            movenet->possibleDevices = {"server"};
+            yolov5n->downstreams.push_back({movenet, 0});
+
+            auto *gender = new PipelineModel{
+                    "server",
+                    "gender",
+                    {},
+                    false,
+                    {},
+                    {},
+                    {},
+                    {{retina1face, -1}}
+            };
+            gender->possibleDevices = {"server"};
+            retina1face->downstreams.push_back({gender, -1});
+
+            auto *age = new PipelineModel{
+                    "server",
+                    "age",
+                    {},
+                    false,
+                    {},
+                    {},
+                    {},
+                    {{retina1face, -1}}
+            };
+            age->possibleDevices = {"server"};
+            retina1face->downstreams.push_back({age, -1});
+
+            auto *sink = new PipelineModel{
+                    "server",
+                    "sink",
+                    {},
+                    false,
+                    {},
+                    {},
+                    {},
+                    {{gender, -1}, {age, -1}, {movenet, -1}}
+            };
+            sink->possibleDevices = {"server"};
+            gender->downstreams.push_back({sink, -1});
+            age->downstreams.push_back({sink, -1});
+            movenet->downstreams.push_back({sink, -1});
+
+            return {datasource, yolov5n, retina1face, movenet, gender, age, sink};
+        }
+        case PipelineType::Video_Call: {
+            auto *datasource = new PipelineModel{startDevice, "datasource", {}, true, {}, {}};
+            datasource->possibleDevices = {startDevice};
+            auto *retina1face = new PipelineModel{
+                    "server",
+                    "retina1face",
+                    {},
+                    true,
+                    {},
+                    {},
+                    {},
+                    {{datasource, -1}}
+            };
+            retina1face->possibleDevices = {startDevice, "server"};
+            datasource->downstreams.push_back({retina1face, -1});
+
+            auto *emotionnet = new PipelineModel{
+                    "server",
+                    "emotionnet",
+                    {},
+                    false,
+                    {},
+                    {},
+                    {},
+                    {{retina1face, -1}}
+            };
+            emotionnet->possibleDevices = {"server"};
+            retina1face->downstreams.push_back({emotionnet, -1});
+
+            auto *age = new PipelineModel{
+                    "server",
+                    "age",
+                    {},
+                    false,
+                    {},
+                    {},
+                    {},
+                    {{retina1face, -1}}
+            };
+            age->possibleDevices = {startDevice, "server"};
+            retina1face->downstreams.push_back({age, -1});
+
+            auto *gender = new PipelineModel{
+                    "server",
+                    "gender",
+                    {},
+                    false,
+                    {},
+                    {},
+                    {},
+                    {{retina1face, -1}}
+            };
+            gender->possibleDevices = {startDevice, "server"};
+            retina1face->downstreams.push_back({gender, -1});
+
+            auto *arcface = new PipelineModel{
+                    "server",
+                    "arcface",
+                    {},
+                    false,
+                    {},
+                    {},
+                    {},
+                    {{retina1face, -1}}
+            };
+            arcface->possibleDevices = {"server"};
+            retina1face->downstreams.push_back({arcface, -1});
+
+            auto *sink = new PipelineModel{
+                    "server",
+                    "sink",
+                    {},
+                    false,
+                    {},
+                    {},
+                    {},
+                    {{emotionnet, -1}, {age, -1}, {gender, -1}, {arcface, -1}}
+            };
+            sink->possibleDevices = {"server"};
+            emotionnet->downstreams.push_back({sink, -1});
+            age->downstreams.push_back({sink, -1});
+            gender->downstreams.push_back({sink, -1});
+            arcface->downstreams.push_back({sink, -1});
+
+            return {datasource, retina1face, emotionnet, age, gender, arcface, sink};
+        }
+        default:
+            return {};
+    }
 }
