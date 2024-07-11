d41c7796e39bd2b2f65f8bb08fe8d89fb90a01d0
tungngreen <29793179+tungngreen@users.noreply.github.com>
2024-07-11 17:20:49 +0900

    Fix: Transfer duration = 0 for models downstream of datasource on edge devices

--- libs/misc/misc.cpp
+++ libs/misc/misc.cpp
@@ -262,7 +262,7 @@ NetworkProfile queryNetworkProfile(
     d2dNetworkProfile.p95PackageSize = res[0]["p95_total_package_size_b"].as<uint32_t>();
 
     if ((senderHost != "server") && (senderHost == receiverHost) && (taskName.find("yolo") != std::string::npos)) {
-        d2dNetworkProfile.p95PackageSize = 0;
+        d2dNetworkProfile.p95TransferDuration = 0;
         return d2dNetworkProfile;
     }
 
