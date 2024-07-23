4e2e6bf2c10b1605048548eb7dd960d6f29abead
tungngreen <29793179+tungngreen@users.noreply.github.com>
2024-07-24 04:34:40 +0900

    Fix: Set device type

--- libs/device_agent/device_agent.cpp
+++ libs/device_agent/device_agent.cpp
@@ -47,6 +47,7 @@ DeviceAgent::DeviceAgent(const std::string &controller_url, const std::string n,
     dev_loggingMode = absl::GetFlag(FLAGS_dev_loggingMode);
     dev_verbose = absl::GetFlag(FLAGS_dev_verbose);
     dev_logPath = absl::GetFlag(FLAGS_dev_logPath);
+    dev_type = type;
 
     dev_metricsServerConfigs.from_json(json::parse(std::ifstream("../jsons/metricsserver.json")));
     dev_metricsServerConfigs.user = "device_agent";
