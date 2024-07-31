170a6d4baa84c90118585d1e5d8471bab8d63737
Lucas Liebe <lucasliebe.ll@gmail.com>
2024-07-31 18:23:07 +0900

    chore: remove temp gpu code

--- libs/controller/controller.cpp
+++ libs/controller/controller.cpp
@@ -645,15 +645,7 @@ void Controller::StartContainer(ContainerHandle *container, bool easy_allocation
     if (container->model == DataSource || container->model == Sink) {
         request.set_device(-1);
     } else {
-        // TODO: REMOVE QUICKFIX after GPU assigning works
-        // request.set_device(container->gpuHandle->number);
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<> dis(0, 3);
-        request.set_device(dis(gen));
-        if (container->device_agent->name != "server") {
-            request.set_device(0);
-        }
+        request.set_device(container->gpuHandle->number);
     }
     request.set_slo(container->inference_deadline);
     request.set_timebudget(container->timeBudgetLeft);
