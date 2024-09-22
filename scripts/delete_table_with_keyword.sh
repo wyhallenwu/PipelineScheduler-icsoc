2edfa6c655365865fce839e7746f70bbf349f52c
Lucas Liebe <lucasliebe.ll@gmail.com>
2024-09-22 15:08:24 +0900

    feat: allow for schema limiting

--- scripts/delete_table_with_keyword.sh
+++ scripts/delete_table_with_keyword.sh
@@ -7,6 +7,7 @@ DEFAULT_PHRASE="default_phrase"
 DEFAULT_DB_NAME="pipeline"
 DEFAULT_DB_USER="postgres"
 DEFAULT_DB_PASSWORD="pipe"
+DEFAULT_SCHEMA=""
 
 # Command line arguments
 DB_HOST=${1:-$DEFAULT_DB_HOST}
@@ -15,15 +16,24 @@ PHRASE=${3:-$DEFAULT_PHRASE}
 DB_NAME=${4:-$DEFAULT_DB_NAME}
 DB_USER=${5:-$DEFAULT_DB_USER}
 DB_PASSWORD=${6:-$DEFAULT_DB_PASSWORD}
+SCHEMA=${7:-$DEFAULT_SCHEMA}
 
 # Export the PGPASSWORD environment variable
 export PGPASSWORD=$DB_PASSWORD
 
 # Get the list of tables containing the phrase in all schemas
-tables=$(psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -t -c "
-SELECT schemaname, tablename 
-FROM pg_tables 
-WHERE tablename LIKE '%$PHRASE%';")
+if [[ -z "$SCHEMA" ]]; then
+  tables=$(psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -t -c "
+  SELECT schemaname, tablename
+  FROM pg_tables 
+  WHERE tablename LIKE '%$PHRASE%';")
+else
+  tables=$(psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -t -c "
+  SELECT schemaname, tablename
+  FROM pg_tables
+  WHERE schemaname = '$SCHEMA' AND tablename LIKE '%$PHRASE%';")
+fi
+
 
 # Loop through the list of tables and drop each one
 while IFS="|" read -r schemaname tablename; do
